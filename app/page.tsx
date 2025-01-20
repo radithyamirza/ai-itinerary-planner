d64872f20723652f503caf42204ece4ef40dd502
radithyamirza00 <radithyamirza00@gmail.com>
2025-01-20 21:02:35 +0700

    fixed globe display

--- app/page.tsx
+++ app/page.tsx
@@ -395,7 +395,7 @@ export default function Page() {
   ];
 
   return (
-    <div className="flex flex-row items-center justify-center py-20 h-screen dark:bg-black bg-white relative w-full">
+    <div className="flex flex-row items-center justify-center py-20 h-screen dark:bg-black bg-white relative w-full space-y-8">
       <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
         <motion.div
           initial={{
@@ -420,7 +420,7 @@ export default function Page() {
           </p>
         </motion.div>
         <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+        <div className="absolute w-full -bottom-20 h-72 md:h-full z-10 space-y-20">
           <World data={sampleArcs} globeConfig={globeConfig} />
         </div>
       </div>
